4601e35a15857259cd94954237be7704f67f3d4e
Keyur Halpati <krhalpati@gmail.com>
2025-10-26 16:45:25 +0530

    add resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = ({ id }) => {
           <span className="word">HALPATI</span>
         </h1>
         <div className="developer-title">Backend Developer</div>
-        <a href="#" className="resume-download">
+        <a href="/src/assets/Keyur_Halpati_Backend_Developer_Resume.pdf" target="_blank" className="resume-download">
           <div className="resume-icon">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
